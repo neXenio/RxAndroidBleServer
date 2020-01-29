2ef29813a149327ff5466ef03b4de07f153d11b4
Steppschuh <register@steppschuh.net>
2020-01-29 16:41:03 +0100

    Implemented equals and hashCode for BaseData

--- rxandroidbleserver/src/main/java/com/nexenio/rxandroidbleserver/service/value/BaseValue.java
+++ rxandroidbleserver/src/main/java/com/nexenio/rxandroidbleserver/service/value/BaseValue.java
@@ -1,5 +1,7 @@
 package com.nexenio.rxandroidbleserver.service.value;
 
+import java.util.Arrays;
+
 import androidx.annotation.NonNull;
 
 public class BaseValue implements RxBleValue {
@@ -30,6 +32,23 @@ public class BaseValue implements RxBleValue {
         return "BaseValue{" +
                 "bytes=0x" + ValueUtil.bytesToHex(bytes) +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        BaseValue baseValue = (BaseValue) o;
+        return Arrays.equals(bytes, baseValue.bytes);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(bytes);
     }
 
 }
