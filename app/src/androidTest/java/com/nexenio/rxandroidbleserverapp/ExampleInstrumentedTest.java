5790164980681718a957414d09541905216dd841
Steppschuh <register@steppschuh.net>
2020-01-30 10:51:57 +0100

    Fixed example instrumentation tests

--- app/src/androidTest/java/com/nexenio/rxandroidbleserverapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/nexenio/rxandroidbleserverapp/ExampleInstrumentedTest.java
@@ -5,25 +5,21 @@ import android.content.Context;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import androidx.test.InstrumentationRegistry;
-import androidx.test.runner.AndroidJUnit4;
-
-import static org.junit.Assert.assertEquals;
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
 
 /**
  * Instrumented test, which will execute on an Android device.
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-@RunWith(AndroidJUnit4.class)
+@RunWith(AndroidJUnit4ClassRunner.class)
 public class ExampleInstrumentedTest {
 
     @Test
     public void useAppContext() {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
-
-        assertEquals("com.nexenio.rxandroidbleserver", appContext.getPackageName());
+        Context appContext = ApplicationProvider.getApplicationContext();
     }
 
 }
